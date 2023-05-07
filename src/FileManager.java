5bcbdee96383d3914e3b84bce1deb1aa49545744
Piotr Śrótwa <altar10420@gmail.com>
2023-05-07 19:13:03 +0200

    implement file sorting metods

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Comparator;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -10,5 +11,16 @@ public class FileManager {
                 .filter(File::isFile) //method reference
                 .map(File::getName)
                 .forEach(System.out::println);
+    }
+
+    public void sortFilesInDirectory(String directory) {
+        Stream.of(Objects.requireNonNull(new File(directory).listFiles()))
+                .filter(File::isFile)
+                .sorted(Comparator.comparingLong(File::length))
+                .forEach(file -> {
+                    System.out.println(file.getName());
+                    System.out.println(file.length());
+                    System.out.println();
+                });
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         FileManager fileManager = new FileManager();
-        fileManager.listFilesInDirectory("/home/gordonfreeman/Pobrane");
+        fileManager.listFilesInDirectory("/");
+        fileManager.sortFilesInDirectory("/");
     }
 }
